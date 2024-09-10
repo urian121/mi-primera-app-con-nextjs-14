22ae6d35142554f9de7f68d7fd9c5323e08b0d38
Urian Viera <urian1213viera@gmail.com>
2024-09-10 13:56:14 -0500

    cambiando url de las imagenes

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  //  reactStrictMode: true,
   images: {
-    domains: ["source.unsplash.com"],
-
-    loader: "default",
-    path: "/_next/image",
-    // Para permitir múltiples dominios remotos
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: ["source.unsplash.com", "fakestoreapi.com"],
-    //     pathname: "**",
-    //   },
-    // ],
-    // En lugar de remotePatterns, puedes usar la propiedad `domains` con Next.js 12+
-    domains: ["source.unsplash.com", "fakestoreapi.com"],
+    domains: ['source.unsplash.com', 'picsum.photos'],
   },
 };
 
